302bd5b93f66a98755dcfbd685627034de88e97b
Fan-Yu Kuan <fykuan@gmail.com>
2014-08-08 12:36:15 +0800

    檔掉 giga-circle

--- no-giga-circle.js
+++ no-giga-circle.js
@@ -1,19 +1,17 @@
-// No Want Want
+// No giga-circle
 // version 0.1
-// Yi-Huan Chan
-// 2014-03-20
+// Fan-Yu Kuan
+// 2014-08-08
 // Released to the public domain.
 //
+// Inspired by no-want-want (https://github.com/yhchan/no-want-want)
 // Inspired by 旺中守門員 (https://chrome.google.com/webstore/detail/%E6%97%BA%E4%B8%AD%E5%AE%88%E9%96%80%E5%93%A1/jgoljbdcdakinkigihjocpniamcgofmm?hl=zh-TW)
 // Inspired by Block Sites (https://userscripts.org/scripts/review/18722)
 //
 // ==UserScript==
-// @name          No Want Want
-// @description   Blocks Want Want related websites
-// @include       /^https?://.*\.chinatimes\.com/.*$/
-// @include       /^https?://.*\.ctitv\.com\.tw/.*$/
-// @include       /^https?://.*\.ctv\.com\.tw/.*$/
-// @include       /^https?://.*\.want-daily\.com/.*$/
+// @name          No giga-circle
+// @description   Blocks gigacircle related websites
+// @include       /^https?://.*\.gigacircle\.com/.*$/
 // @grant         none
 // ==/UserScript==
 
